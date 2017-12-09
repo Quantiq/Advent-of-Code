ef8b01ca5fb471239509205e9d1bcc81be3aa30d
Quantiq <allardae@gmail.com>
2017-12-09 11:42:47 -0500

    added day 8

---
+++ Day 8/8.1.py
@@ -0,0 +1,48 @@
+def main():
+    with open("input.txt") as f:
+        content = [i.split() for i in f.read().splitlines()]
+
+    registers = {}
+
+    for lines in content:
+        registers[lines[0]] = 0
+
+    for lines in content:
+        key_to_change = lines[0]
+        add_sub = lines[1]
+        num_to_change_by = int(lines[2])
+        
+        condition_key = lines[4]
+        condition = lines[5]
+        condition_num = int(lines[6])
+
+        if evaluate_condition(registers, condition_key, condition, condition_num):
+            registers = update_registers(registers, key_to_change, add_sub, num_to_change_by)
+
+    print(max(registers.values()))
+
+def evaluate_condition(registers, key, condition, num):
+    if condition == '==':
+        return registers[key] == num
+    elif condition == '!=':
+        return registers[key] != num
+    elif condition == '<':
+        return registers[key] < num
+    elif condition == '<=':
+        return registers[key] <= num
+    elif condition == '>':
+        return registers[key] > num
+    elif condition == '>=':
+        return registers[key] >= num
+
+
+def update_registers(registers, key, add_sub, num):
+    if add_sub == 'inc':
+        registers[key] += num
+    elif add_sub == 'dec':
+        registers[key] -= num
+    return registers
+
+
+if __name__ == '__main__':
+    main()
