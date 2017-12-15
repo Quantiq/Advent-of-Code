3f9783e425db471ee0f46b5022acc1d93ff017c3
Quantiq <allardae@gmail.com>
2017-12-15 03:13:11 -0500

    added day 15

---
+++ Day 15/15.1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+
+    count = 0
+
+    for i in range(40000000):
+        a = (a * 16807) % 2147483647
+        b = (b * 48271) % 2147483647
+
+        if (a & 0xffff) == (b & 0xffff):
+            count += 1
+
+    print(count)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Day 15/15.2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+
+    count = 0
+
+    for i in range(5000000):
+        while True:
+            a = (a * 16807) % 2147483647
+            if a % 4 == 0:
+                break
+
+        while True:
+            b = (b * 48271) % 2147483647
+            if b % 8 == 0:
+                break
+
+        if (a & 0xffff) == (b & 0xffff):
+            count += 1
+
+    print(count)
+
+
+if __name__ == '__main__':
+    main()
